c4d26b407fff279821ea02841ac38e374e1ae7aa
Yalor1 <42723734+Yalor1@users.noreply.github.com>
2018-12-04 09:28:27 -0800

    Update ConfigurationSpace.java

--- src/ConfigurationSpace.java
+++ src/ConfigurationSpace.java
@@ -1,7 +1,7 @@
 
 public interface ConfigurationSpace {
-	public static final int BOARD_WIDTH = 2000;
-	public static final int BOARD_HEIGHT = 1000;
+	public static final int BOARD_WIDTH = 1370;
+	public static final int BOARD_HEIGHT = 730;
 	public static final int AIRPLANE_HEIGHT = 230;
 	public static final int AIRPLANE_WIDTH = 290;
 	public static final int BORDER_RIGHT = 30;
@@ -10,5 +10,5 @@ public interface ConfigurationSpace {
 	public static final int DELAY = 17;
 	public static final int DRONE_WIDTH = 150;
 	public static final int DRONE_HEIGHT = 250;
-
+	public static final int BROWN_LAND = 700;
 }
