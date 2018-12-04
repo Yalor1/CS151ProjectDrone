fb96d2902052f456769a37df5711b9df4cf30f7a
Yalor1 <42723734+Yalor1@users.noreply.github.com>
2018-12-04 09:19:06 -0800

    Update Timer.java

--- src/Timer.java
+++ src/Timer.java
@@ -1,12 +1,44 @@
-import javax.swing.Timer;
 
+import javax.swing.*;
 
-private Timer timer;
+public class GameTimer  extends JLabel{
 
+	private int	seconds, minutes;
+	private Timer timer;
 
-public class Timer {
+	public GameTimer()
+	{
+		seconds = 0;
+		minutes = 0;
 
-	
-	timer = new Timer(DELAY, this);
-	timer.start();
+		timer = new Timer(1000, event ->{
+			this.increment();
+			this.setText(this.toString());
+		});
+	}
+
+	public void start()
+	{
+		timer.start();
+	}
+
+	public void stop()
+	{
+		timer.stop();
+	}
+
+	public void increment()
+	{
+		seconds++;
+		if (seconds >= 60 )
+		{
+			minutes++;
+			seconds = 0;
+		}
+	}
+
+	public String toString()
+	{
+		return String.format("%02d:%02d", minutes, seconds);
+	}
 }
